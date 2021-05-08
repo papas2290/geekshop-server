d56f95e613b6a8b6479e69b1cfa17dace1025b9b
Anton_Kiryushin <papas2290@mail.ru>
2021-05-08 07:45:50 +0200

    added checking in base.html

--- authapp/views.py
+++ authapp/views.py
@@ -30,3 +30,8 @@ def register(request):
         form = UserRegisterForm()
     context = {'title': 'GeekShop регистрация', 'form': form}
     return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
