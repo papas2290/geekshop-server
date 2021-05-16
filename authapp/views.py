3f33356e7685abdddeb7773f085259d9a87bf541
Anton_Kiryushin <papas2290@mail.ru>
2021-05-16 18:55:08 +0200

    added messages in register and login

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from authapp.forms import UserLoginForm, UserRegisterForm
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 
@@ -25,6 +25,7 @@ def register(request):
         form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегестрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegisterForm()
