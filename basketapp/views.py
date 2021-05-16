a507442cfbc12db21662906840fca8f095883a5f
Anton_Kiryushin <papas2290@mail.ru>
2021-05-16 22:54:45 +0200

    added @login_required for basket

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import HttpResponseRedirect
 from mainapp.models import Product
 from basketapp.models import Basket
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def basket_add(request, product_id=None):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -17,6 +19,7 @@ def basket_add(request, product_id=None):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, id):
     Basket.objects.get(id=id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
