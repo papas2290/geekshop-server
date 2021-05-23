a367133ba787a297d434acc0ef270f2df0307a79
Anton_Kiryushin <papas2290@mail.ru>
2021-05-23 13:39:55 +0200

    added read users

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from adminapp.views import index
+from adminapp.views import index, admin_users_read
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('admin_users_read', admin_users_read, name='admin_users_read'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from authapp.models import User
 
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
+def admin_users_read(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admin_users_read.html', context)
