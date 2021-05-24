95ddf0e2c15b973d5e8955e8168cdcaf10a48d4b
Anton_Kiryushin <papas2290@mail.ru>
2021-05-24 23:05:50 +0200

    added create users in admin_staff

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from adminapp.views import index, admin_users_read
+from adminapp.views import index, admin_users_read, admin_users_create
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('admin_users_read', admin_users_read, name='admin_users_read'),
+    path('admin_users_read/', admin_users_read, name='admin_users_read'),
+    path('admin_users_create/', admin_users_create, name='admin_users_create'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
+
+
 
 def index(request):
     return render(request, 'adminapp/admin.html')
@@ -8,3 +12,15 @@ def index(request):
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin_users_read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin_users_create.html', context)
