b33329b195ff3b62c6be55d1b1570b4459477112
Anton_Kiryushin <papas2290@mail.ru>
2021-05-27 10:16:53 +0200

    modified adminapp/vies.py

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminPrifileForm
@@ -17,12 +19,20 @@ class UserListView(ListView):
     model = User
     template_name = 'adminapp/admin_users_read.html'
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
 
 class UserCreateView(CreateView):
     model = User
     template_name = 'adminapp/admin_users_create.html'
     form_class = UserAdminRegisterForm
     success_url = reverse_lazy('admin_staff:admin_users_read')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserUpdateView(UpdateView):
@@ -32,7 +42,26 @@ class UserUpdateView(UpdateView):
     form_class = UserAdminPrifileForm
 
 
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop Admin- Редактирование пользователя'
+        return context
+
+
+
+
 class UserDeleteView(DeleteView):
     model = User
     template_name = 'adminapp/admin_users_update_delete.html'
     success_url = reverse_lazy('admin_staff:admin_users_read')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(success_url)
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
