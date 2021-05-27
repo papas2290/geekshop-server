08c2a059e63381088ff6b0ca8ecb6f570294756e
Anton_Kiryushin <papas2290@mail.ru>
2021-05-27 08:00:25 +0200

    added filtering by category

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,10 +13,11 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'Страница товаров',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+def products(request, category_id=None):
+    context = {'title': 'Geek Shop каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+        context.update({'products': products})
+    else:
+        context.update({'products': Product.objects.all()})
     return render(request, 'mainapp/products.html',  context)
